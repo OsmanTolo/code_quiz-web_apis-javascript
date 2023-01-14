8668d8ba66b1979c0e36bab2bf1dc0afef1fa87b
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-14 17:28:42 +0000

    Add questions with their answers and explanation

---
+++ assets/js/questions.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let questions = [
+  {
+    question1: "Javascript is an _______ language?",
+    answers1: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above",
+    ],
+    correctAns1: answers[1],
+    explanation1: "Javascript is an Object-Oriented Language.",
+  },
+  {
+    question2:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    answers2: [
+      "getElementById()",
+      "querySelector()",
+      "Both A and B",
+      "Noe of the Above",
+    ],
+    correctAns2: answers[2],
+    explanation2:
+      "Both the above methods are used to access HTML elements using Javascript.",
+  },
+  {
+    question3: "How can a datatype be declared to be a constant type?",
+    answers3: ["const", "var", "let", "constant"],
+    correctAns3: answers[0],
+    explanation3:
+      "The const keyword is used to declare a variable as a constant type in Javascript and tells the compiler that its value cannot be changed later in the program.",
+  },
+  {
+    question4:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    answers4: ["var", "let", "Both A and B", "None of the above"],
+    correctAns4: answers[2],
+    explanation4:
+      "Both var and let keywords are used to define a variable in JavaScript",
+  },
+  {
+    question5:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    answers5: ["var", "let", "Both A and B", "None of the above"],
+    correctAns5: answers[2],
+    explanation5:
+      "Both var and let keywords are used to define a variable in JavaScript",
+  },
+];
