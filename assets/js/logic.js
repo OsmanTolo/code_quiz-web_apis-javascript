4dd9609180ccf92694d79c70a238cfcb16fb1829
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-14 19:11:08 +0000

    Add function to display the question and choices
    Within the starBtn event listener:
     * Implemented a callback fn that adds and remove the 'hide' class to display the questions and remove the start screen
     * Called the displayQuestion fn to display the first question and choices
      * Within this fn, utilised a for loop to append the ol of choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,41 @@
-console.log(questions);
+const startScreenEl = document.querySelector("#start-screen");
+const startBtnEl = document.querySelector("#start");
+const questionsEl = document.querySelector("#questions");
+const questionTitleEl = document.querySelector("#question-title");
+const questionChoicesEl = document.querySelector("#choices");
+
+console.log(questions[0].question0.question);
+
+function displayQuestions() {
+  questionTitleEl.textContent = questions[0].question0.question;
+  let olEl = document.createElement("ol");
+  questionsEl.appendChild(olEl);
+  let choicesArr = questions[0].question0.choices;
+  for (let i = 0; i < choicesArr.length; i++) {
+    let liEl = document.createElement("li");
+    olEl.appendChild(liEl);
+    liEl.textContent = choicesArr[i];
+  }
+  // liEl.textContent = questions[0].question0.choices;
+  // olEl.textContent = element[key].question;
+
+  // for (let i = 0; i < questions.length; i++) {
+  //   const element = questions[i];
+  //   for (const key in element) {
+  //     console.log(element[key].question);
+  //     // console.log(element[key].choices);
+  //     // console.log(element[key].solution());
+  //     // olEl.textContent = element[key].question;
+  //     // questionsEl.appendChild(olEl);
+  //   }
+  // }
+}
+
+startBtnEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Hide the start screen
+  startScreenEl.classList.add("hide");
+  // Display the question screen
+  questionsEl.classList.remove("hide");
+  displayQuestions();
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,59 +2,59 @@
 
 let questions = [
   {
-    question1: {
+    question0: {
       question: "Javascript is an _______ language?",
-      answers: [
+      choices: [
         "Object-Oriented",
         "Object-Based",
         "Procedural",
         "None of the above",
       ],
       solution() {
-        return this.answers[1];
+        return this.choices[1];
       },
     },
   },
   {
-    question2: {
+    question1: {
       question:
         "Which of the following methods is used to access HTML elements using Javascript?",
-      answers: [
+      choices: [
         "getElementById()",
         "querySelector()",
         "Both A and B",
         "Noe of the Above",
       ],
       solution() {
-        return this.answers[2];
+        return this.choices[2];
+      },
+    },
+  },
+  {
+    question2: {
+      question: "How can a datatype be declared to be a constant type?",
+      choices: ["const", "var", "let", "constant"],
+      solution() {
+        return this.choices[0];
       },
     },
   },
   {
     question3: {
-      question: "How can a datatype be declared to be a constant type?",
-      answers: ["const", "var", "let", "constant"],
+      question:
+        "Which of the following keywords is used to define a variable in Javascript?",
+      choices: ["const", "var", "let", "constant"],
       solution() {
-        return this.answers[0];
+        return this.choices[2];
       },
     },
   },
   {
     question4: {
-      question:
-        "Which of the following keywords is used to define a variable in Javascript?",
-      answers: ["const", "var", "let", "constant"],
-      solution() {
-        return this.answers[2];
-      },
-    },
-  },
-  {
-    question5: {
       question: "What will be the output of print(typeof(NaN))?",
-      answers: ["Object", "Number", "String", "None of the above"],
+      choices: ["Object", "Number", "String", "None of the above"],
       solution() {
-        return this.answers[1];
+        return this.choices[1];
       },
     },
   },
