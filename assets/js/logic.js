fc373526c9521659721583f482897910cfd1fbc9
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-15 20:44:49 +0000

    Iterated the question.js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,78 @@
-const startScreenEl = document.querySelector("#start-screen");
+"use strict";
+
+const startScreenContainerEl = document.querySelector("#start-screen");
 const startBtnEl = document.querySelector("#start");
-const questionsEl = document.querySelector("#questions");
+const questionsContainerEl = document.querySelector("#questions");
 const questionTitleEl = document.querySelector("#question-title");
 const questionChoicesEl = document.querySelector("#choices");
 
-console.log(questions[0].question0.question);
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
+// console.log(questions[0].question);
 
+startBtnEl.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  // Hide the start screen
+  startScreenContainerEl.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  // Display the questions container
+  questionsContainerEl.classList.remove("hide");
+  SetNextQuestion();
+}
+
+function SetNextQuestion() {
+  // Get the question - show the question at the current shuffledQuestion index
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionTitleEl.textContent = question.question;
+
+  let olEl = document.createElement("ol");
+  questionsContainerEl.appendChild(olEl);
+
+  // Loop through the choices and add them to the screen
+  question.choices.forEach((choice, choiceIndex) => {
+    let liEl = document.createElement("li");
+    let choiceBtnEl = document.createElement("button");
+    olEl.appendChild(liEl);
+    liEl.appendChild(choiceBtnEl);
+    choiceBtnEl.textContent = choice;
+
+    choiceBtnEl.addEventListener("click", function () {
+      if (choiceIndex === question.solution) {
+        console.log(`Correct Answer`);
+        score += 5;
+        SetNextQuestion();
+      } else {
+        console.log(`Wrong Answer`);
+        if (score === 0) {
+          score;
+        } else {
+          score -= 5;
+        }
+        SetNextQuestion();
+      }
+    });
+  });
+}
+
+function selectChoice() {}
+
+/*
 function displayQuestions() {
+  // Display the question screen
+  questionsContainerEl.classList.remove("hide");
   questionTitleEl.textContent = questions[0].question0.question;
   let olEl = document.createElement("ol");
-  questionsEl.appendChild(olEl);
+  questionsContainerEl.appendChild(olEl);
   let choicesArr = questions[0].question0.choices;
   for (let i = 0; i < choicesArr.length; i++) {
     let liEl = document.createElement("li");
     olEl.appendChild(liEl);
-    liEl.textContent = choicesArr[i];
+    liEl.innerHTML = choicesArr[i];
   }
   // liEl.textContent = questions[0].question0.choices;
   // olEl.textContent = element[key].question;
@@ -26,16 +84,8 @@ function displayQuestions() {
   //     // console.log(element[key].choices);
   //     // console.log(element[key].solution());
   //     // olEl.textContent = element[key].question;
-  //     // questionsEl.appendChild(olEl);
+  //     // questionsContainerEl.appendChild(olEl);
   //   }
   // }
 }
-
-startBtnEl.addEventListener("click", function (e) {
-  e.preventDefault();
-  // Hide the start screen
-  startScreenEl.classList.add("hide");
-  // Display the question screen
-  questionsEl.classList.remove("hide");
-  displayQuestions();
-});
+*/
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,61 +2,56 @@
 
 let questions = [
   {
-    question0: {
-      question: "Javascript is an _______ language?",
-      choices: [
-        "Object-Oriented",
-        "Object-Based",
-        "Procedural",
-        "None of the above",
-      ],
-      solution() {
-        return this.choices[1];
-      },
-    },
+    question: "Javascript is an _______ language?",
+    choices: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above",
+    ],
+    solution: 0,
+    // solution() {
+    //   return this.choices[1];
+    // },
   },
   {
-    question1: {
-      question:
-        "Which of the following methods is used to access HTML elements using Javascript?",
-      choices: [
-        "getElementById()",
-        "querySelector()",
-        "Both A and B",
-        "Noe of the Above",
-      ],
-      solution() {
-        return this.choices[2];
-      },
-    },
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    choices: [
+      "getElementById()",
+      "querySelector()",
+      "Both A and B",
+      "Noe of the Above",
+    ],
+    solution: 2,
+    // solution() {
+    //   return this.choices[2];
+    // },
   },
   {
-    question2: {
-      question: "How can a datatype be declared to be a constant type?",
-      choices: ["const", "var", "let", "constant"],
-      solution() {
-        return this.choices[0];
-      },
-    },
+    question: "How can a datatype be declared to be a constant type?",
+    choices: ["const", "var", "let", "constant"],
+    solution: 0,
+    // solution() {
+    //   return this.choices[0];
+    // },
   },
   {
-    question3: {
-      question:
-        "Which of the following keywords is used to define a variable in Javascript?",
-      choices: ["const", "var", "let", "constant"],
-      solution() {
-        return this.choices[2];
-      },
-    },
+    question:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    choices: ["const", "var", "let", "All of the above"],
+    solution: 3,
+    // solution() {
+    //   return this.choices[2];
+    // },
   },
   {
-    question4: {
-      question: "What will be the output of print(typeof(NaN))?",
-      choices: ["Object", "Number", "String", "None of the above"],
-      solution() {
-        return this.choices[1];
-      },
-    },
+    question: "What will be the output of print(typeof(NaN))?",
+    choices: ["Object", "Number", "String", "None of the above"],
+    solution: 1,
+    // solution() {
+    //   return this.choices[1];
+    // },
   },
 ];
 
